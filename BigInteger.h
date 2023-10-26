869f991280a5e961195598e20745cf4d2dc2e21b
Ankit-Manwal <149080283+Ankit-Manwal@users.noreply.github.com>
2023-10-26 21:30:53 +0530

    Update BigInteger.h

--- BigInteger.h
+++ BigInteger.h
@@ -12,17 +12,16 @@ struct BigInteger
     char sign;
 };
 
-struct node* insert(struct node* head, int val);
-void insert_end(struct BigInteger* n, int element);
+struct node* insert_node(struct node* head, int val);
+struct BigInteger initialize(char* s);
+void insert_big_end(struct BigInteger* n, int val) ;
 int length(struct node* head);
 struct node* reverse(struct node* head);
 int compare(struct BigInteger n1, struct BigInteger n2);
-struct BigInteger initialize(char* s);
 void trailing(struct node ** head);
 struct BigInteger add(struct BigInteger n1, struct BigInteger n2);
 struct BigInteger sub(struct BigInteger n1, struct BigInteger n2);
 struct BigInteger mul(struct BigInteger n1, struct BigInteger n2);
 void displaylinked(struct node* head);
-void display(struct BigInteger n);
 void display(struct BigInteger n);
 void free_linked(struct BigInteger* n);
